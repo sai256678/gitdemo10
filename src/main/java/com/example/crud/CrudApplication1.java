935528d894e9b6693eea46175462036d437cb533
saikumar <annapuramsaikumar@gmail.com>
2025-06-18 19:53:27 +0530

    this is commit from second application

--- src/main/java/com/example/crud/CrudApplication1.java
+++ src/main/java/com/example/crud/CrudApplication1.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudApplication {
+public class CrudApplication1 {
     public static void main(String[] args) {
-        SpringApplication.run(CrudApplication.class, args);
+        SpringApplication.run(CrudApplication1.class, args);
     }
 }
